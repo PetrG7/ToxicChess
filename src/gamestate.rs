81891a7795f04b620ef6db6f7bfe21690f90f969
PierreGordo <peter.gardas@gmail.com>
2026-04-23 12:49:18 +0200

    Basic work - gotta go

--- src/gamestate.rs
+++ src/gamestate.rs
@@ -1,3 +1,5 @@
+use std::fmt; // for implementing display
+
 //structure to hold all the necesarry bitboards
 #[derive(Default, Debug)]
 pub struct GameState {
@@ -18,11 +20,42 @@ pub struct GameState {
 	black_pieces: u64,
 }
 
+//helping enum for adding pieces
+pub enum Colour {
+	Black,
+	White,
+}
+
+impl fmt::Display for Colour {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		match self {
+			Colour::Black => write!(f, "Black"),
+			Colour::White => write!(f, "White"),
+		}
+	}
+}
+
 
 //implementing structures to add pieces
 impl GameState{
 	//here it is probably best to use 1-8 for coords instead of 0-7, because its easiers
-	pub fn add(piece_type: &'static str, colour: &'static str, x: u8, y: u8){
+	pub fn add(piece_type: &'static str, colour: Colour, x: u8, y: u8){
+		//check whether x and y are possible values - colour is safe, because it is an enum, piece_type will be handled by matching
+		if x > 7 || y > 7 {
+			//don't know whether it is better to handle the error here, or later with this function returning option.
+			println!("Invalid piece coordinates for: {} at coords x: {}, y: {}, colour is {}. Piece was not added.", piece_type, x, y, colour);
+		}
+		//convert input to lowercase
+		match piece_type.to_lowercase().as_str() {
+			"pawn" => {},
+			"knight" => {},
+			"bishop" => {},
+			"rook" => {},
+			"queen" => {},
+			"king" => {},
+			_ => {},
+		}
+		
 		todo!();
 	}
 
@@ -30,6 +63,35 @@ impl GameState{
 	pub fn new() -> Self {
 		GameState::default()
 	}
+	
+	//function to add pawn
+	fn add_pawn(&mut self, x: u8, y: u8, colour: Colour) {
+		match colour {
+			Colour::White => {
+				//check whether there is not a piece at that certain index
+				if Self::is_bit_set(self.white_pieces, Self::get_index(x, y)) {
+					//there is already a piece there
+					println!("These is already a pawn at x: {}, y: {}, the pawn has not been added.", x, y);
+				}
+				else{
+					self.white_pawns |= 1u64 << Self::get_index(x, y);
+				}
+			}
+			Colour::Black => {
 
+				
+			}
+		}
+	}
+
+	//calculate index of u64 bit from coords
+	fn get_index(x: u8, y: u8) -> u8 {
+		(x+1)*(y+1)
+	}
+
+	//helper function to calculate whether a bit at a certain index is set - it is 1
+	fn is_bit_set(number: u64, index: u8) -> bool {
+		(number & (1u64 << index)) != 0
+	}
 	
 }
